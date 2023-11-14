e70dbce64be23110724e76ed32e027c56ecf51b4
hadiplays <hadi.baajour@ufl.edu>
2023-11-13 22:42:07 -0500

    changed functions

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -9,28 +9,28 @@ namespace EsepWebhook;
 
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-      public string FunctionHandler(object input, ILambdaContext context)
-    {
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-        
-        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-        
-        var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
-        {
-            Content = new StringContent(payload, Encoding.UTF8, "application/json")
-        };
 
-        var response = client.Send(webRequest);
-        using var reader = new StreamReader(response.Content.ReadAsStream());
-            
-        return reader.ReadToEnd();
-    }
+  /// <summary>
+  /// A simple function that takes a string and does a ToUpper
+  /// </summary>
+  /// <param name="input"></param>
+  /// <param name="context"></param>
+  /// <returns></returns>
+  public string FunctionHandler(object input, ILambdaContext context)
+  {
+    dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+
+    string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+
+    var client = new HttpClient();
+    var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+    {
+      Content = new StringContent(payload, Encoding.UTF8, "application/json")
+    };
+
+    var response = client.Send(webRequest);
+    using var reader = new StreamReader(response.Content.ReadAsStream());
+
+    return reader.ReadToEnd();
+  }
 }
